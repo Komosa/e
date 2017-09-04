4f00b8c52e3dd6ad2b29761dbc78dbac649ea9a7
Przemek Komosa <przemek.komosa@gmail.com>
2017-09-04 21:12:49 +0200

    Print comment for failed tests

--- e_test.go
+++ e_test.go
@@ -8,29 +8,30 @@ import (
 )
 
 func Test(t *testing.T) {
-	f := func(in string, exp string) {
+	f := func(in string, exp string, comment string) {
 		out := e.Process(emit(in))
 		if out != exp {
-			t.Errorf("for %q:\n\tgot %q but\n\texpected %q", in, out, exp)
+			t.Errorf("for %q:\n\tgot %q but\n\texpected %q\ncomment: %s", in, out, exp, comment)
 		}
 	}
 
-	f("", "")
-	f("a", "a")                    // entering text
-	f("a←b", "ba")                 // going back
-	f("←a", "a")                   // going on left border
-	f("a←→b", "ab")                // going back and forth
-	f("a→b", "ab")                 // going on right border
-	f("a\nb", "a\nb")              // entering new lines
-	f("a\n↑b", "ba\n")             // going up (as in nano and geany)
-	f("ab\nc↑d", "adb\nc")         // entering in previous lines
-	f("a↑b", "ba")                 // going up in first line
-	f("a\n↑b↓c", "ba\nc")          // going down
-	f("a←↓b", "ab")                // going down in last line
-	f("↑a", "a")                   // going up at first char
-	f("b\nac↑f", "bf\nac")         // going up to shorter line
-	f("ab\nabcde↑f", "abf\nabcde") // going up to shorter line
-	f("\nb↑d", "d\nb")             // starting with newline
+	f("", "", "empty data")
+	f("a", "a", "entering text")
+	f("a←b", "ba", "going back")
+	f("←a", "a", "going on left border")
+	f("a←→b", "ab", "going back and forth")
+	f("a→b", "ab", "going on right border")
+	f("a\nb", "a\nb", "entering new lines")
+	f("a\n↑b", "ba\n", "going up (as in nano and geany)")
+	f("ab\nc↑d", "adb\nc", "entering in previous lines")
+	f("a↑b", "ba", "going up in first line")
+	f("a\n↑b↓c", "ba\nc", "going down")
+	f("a←↓b", "ab", "going down in last line")
+	f("↑a", "a", "going up at first char")
+	f("b\nac↑f", "bf\nac", "going up to shorter line")
+	f("ab\nabcde↑f", "abf\nabcde", "going up to shorter line")
+	f("\nb↑d", "d\nb", "starting with newline")
+	//~ f("ab\n\nc↑↑d", "adb\n\nc", "going up to preffered column instead of shortest one along the way")
 }
 
 func TestFuzz(t *testing.T) {
